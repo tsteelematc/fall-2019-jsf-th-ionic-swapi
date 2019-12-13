ec309fddf4ed262d35f07634bd79b4eaad48cbe8
tsteelematc <tsteele@madisoncollege.edu>
2019-12-12 19:21:11 -0600

    Hard coding 61 planets : - (

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { merge } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,17 @@ export class SwapiService {
   getPlanets() {
     
     const o1 = this.httpSvc.get('https://swapi.co/api/planets/');
-    return o1;
+    //return o1;
+
+    const o2 = this.httpSvc.get('https://swapi.co/api/planets?page=2');
+    const o3 = this.httpSvc.get('https://swapi.co/api/planets?page=3');
+    const o4 = this.httpSvc.get('https://swapi.co/api/planets?page=4');
+    const o5 = this.httpSvc.get('https://swapi.co/api/planets?page=5');
+    const o6 = this.httpSvc.get('https://swapi.co/api/planets?page=6');
+    const o7 = this.httpSvc.get('https://swapi.co/api/planets?page=7');
+
+    return merge(o1, o2, o3, o4, o5, o6, o7);
+
 
   }
 }
