2dce4a136843a7d677368426fe7323a403fe9a36
tsteelematc <tsteele@madisoncollege.edu>
2019-12-12 18:13:22 -0600

    First part of demo done...

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SharedDataService } from '../shared-data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private sharedDataSvc: SharedDataService) {}
+
+}
